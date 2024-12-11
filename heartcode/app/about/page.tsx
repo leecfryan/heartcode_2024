85a44e20e6dc1c04840a0ff5a80dba5aedf45842
NeoJunciMegan <neo_junci_megan@students.edu.sg>
2024-12-11 13:07:49 +0800

    add compare component

--- heartcode/app/about/page.tsx
+++ heartcode/app/about/page.tsx
@@ -9,6 +9,8 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Compare } from "@/components/ui/compare";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export default function Home() {
   return (
@@ -24,9 +26,9 @@ export default function Home() {
             <TabsTrigger value="cheyenne">Cheyenne</TabsTrigger>
           </TabsList>
           <TabsContent value="megan">
-            <Card className="bg-stone-300">
+            <Card className="bg-zinc-300 dark:bg-zinc-800 shadow-red-500 drop-shadow-2xl mb-32 ">
               <CardHeader>
-                <CardTitle>Hi! I am Megan</CardTitle>
+                <CardTitle className=" text-2xl">Hi! I am Megan.</CardTitle>
                 <CardDescription>
                   I LOVE CATS AND I LOVE CATS AND I LOVE CATS AND I LOVE
                   CATSSSSSS!!!!!!
@@ -34,15 +36,15 @@ export default function Home() {
               </CardHeader>
               <CardContent className="space-y-2">
                 <div className="space-y-1">
-                  <Label htmlFor="name">Name</Label>
+                  <Label className="text-xl" htmlFor="name">Name</Label>
                   <Input id="name" defaultValue="Megan" disabled />
                 </div>
                 <div className="space-y-1">
-                  <Label htmlFor="username">Age</Label>
+                  <Label className="text-xl" htmlFor="username">Age</Label>
                   <Input id="username" defaultValue="15" disabled />
                 </div>
                 <div className="space-y-1">
-                  <Label htmlFor="username">School</Label>
+                  <Label className="text-xl" htmlFor="username">School</Label>
                   <Input
                     id="username"
                     defaultValue="Yuan Ching Secondary School"
@@ -50,16 +52,16 @@ export default function Home() {
                   />
                 </div>
                 <div className="space-y-1">
-                  <Label htmlFor="username">Fun Fact</Label>
+                  <Label className="text-xl" htmlFor="username">Fun Fact</Label>
                   <Input id="username" defaultValue="idk" disabled />
                 </div>
               </CardContent>
             </Card>
           </TabsContent>
           <TabsContent value="cheyenne">
-            <Card className="bg-gray-300">
+            <Card className="bg-gray-300 dark:bg-gray-800 mb-32">
               <CardHeader>
-                <CardTitle>Hi I am Cheyenne. I play with everything</CardTitle>
+                <CardTitle className="text-2xl">Hi! I am Cheyenne. I play with everything.</CardTitle>
                 <CardDescription>
                   I love singing, drawing, i play minecraft, roblox, path to
                   nowhere and project sekai
@@ -67,15 +69,15 @@ export default function Home() {
               </CardHeader>
               <CardContent className="space-y-2">
                 <div className="space-y-1">
-                  <Label htmlFor="name">Name</Label>
+                  <Label className="text-xl" htmlFor="name">Name</Label>
                   <Input id="name" defaultValue="Cheyenne" disabled />
                 </div>
                 <div className="space-y-1">
-                  <Label htmlFor="username">Age</Label>
+                  <Label className="text-xl" htmlFor="username">Age</Label>
                   <Input id="username" defaultValue="15" disabled />
                 </div>
                 <div className="space-y-1">
-                  <Label htmlFor="username">School</Label>
+                  <Label className="text-xl" htmlFor="username">School</Label>
                   <Input
                     id="username"
                     defaultValue="Geylang Methodist Secondary School"
@@ -83,7 +85,7 @@ export default function Home() {
                   />
                 </div>
                 <div className="space-y-1">
-                  <Label htmlFor="username">Fun Fact</Label>
+                  <Label className="text-xl" htmlFor="username">Fun Fact</Label>
                   <Input
                     id="username"
                     defaultValue="i love amath (REAL)"
@@ -95,6 +97,17 @@ export default function Home() {
           </TabsContent>
         </Tabs>
       </div>
+
+      <div className="p-4 border rounded-3xl dark:bg-neutral-900 bg-neutral-100  border-neutral-200 dark:border-neutral-800 px-4">
+      <Compare
+        firstImage="https://assets.aceternity.com/code-problem.png"
+        secondImage="https://assets.aceternity.com/code-solution.png"
+        firstImageClassName="object-cover object-left-top"
+        secondImageClassname="object-cover object-left-top"
+        className="h-[250px] w-[200px] md:h-[500px] md:w-[500px]"
+        slideMode="hover"
+      />
+    </div>
     </div>
   );
 }
