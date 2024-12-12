bf4517d049426c7abfbc4a92428294a85a3ba80d
ruckyluv168 <61502605+leecfryan@users.noreply.github.com>
2024-12-12 10:27:37 +0800

    Update page.tsx

--- heartcode/app/page.tsx
+++ heartcode/app/page.tsx
@@ -57,7 +57,7 @@ const cards = [
 
 export default function Home() {
   return (
-    <div className="w-[calc(100vw-50px)] font-mono">
+    <div className="lg:w-[calc(100vw-50px)] md:w-screen font-mono">
       <div className="h-lvh relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
         <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
         <Boxes />
