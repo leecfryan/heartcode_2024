a42e73ac225834d613c1bfafb725ffa9c2c52a13
ruckyluv168 <61502605+leecfryan@users.noreply.github.com>
2024-12-13 12:45:09 +0800

    Update middleware.ts

--- heartcode/middleware.ts
+++ heartcode/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/','/about'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/','/about', '/resource'])
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
